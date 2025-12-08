27fa69ac3d67f154e4d5f185d8e162880d5d6566
Leon Magbanua <112917169+MagbanLeon@users.noreply.github.com>
2025-12-08 23:36:09 +0000

    more changes

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 # idk 2
 # https://docs.github.com/en/codespaces/developing-in-a-codespace/forwarding-ports-in-your-codespace
+# it didnt work becuase you didnt pip install flask DUMBASS
 
-from flask import Flask
+from flask import Flask, render_template, g
+from models import get_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    cur = get_db().cursor()
+    return render_template('landing.html')
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 app.run(debug=True)
